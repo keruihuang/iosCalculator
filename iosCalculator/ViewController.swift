a04549731bed959ef4b4dddd7bbde924cda80f3b
Kerui Huang <kh24@ad.rice.edu@dnc-01-mac.local>
2016-10-03 12:41:43 -0500

    first class

--- iosCalculator/ViewController.swift
+++ iosCalculator/ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var display: UILabel!
     
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
+    }
 
+    @IBAction func pi(sender: UIButton) {
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "pi"{
+                display.text = String(M_PI)
+            }
+        }
+    }
 }
 
